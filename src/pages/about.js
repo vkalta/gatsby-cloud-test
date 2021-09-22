8c23fdd28cbc192771c3bcfb136c557be95001d7
Vikram Kalta <vikram.kalta@e9.co.uk>
2021-09-22 12:06:28 +0530

    chore: added /about screen

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,55 @@
-import React from "react"
-export default function About() {
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from '../components/layout';
+
+export default function About({ data }) {
+  console.log('data--->', JSON.stringify(data));
+  const blog = data.allContentstackBlog.edges;
+  console.log('blog--->', blog);
   return (
-    <div>
-      <h1>About me</h1>
-      <p>
-        I’m good enough, I’m smart enough, and gosh darn it, people like me!
-      </p>
-    </div>
+    <Layout>
+      <h1>Replica of index</h1>
+      <div style={{width:'500px', height:'100px', background: 'yellow'}}>
+        {blog.map((el, i) => {
+          const { node } = el;
+          return (
+            <div key={i}>
+              <p>{node.owner.author_name}</p>
+              {node.authors.map((author, i1) => (
+                <div key={i1}>
+                  <span>{author.first_name}</span>
+                  <span>{author.last_name}</span>
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
   )
 }
+
+export const pageQuery = graphql`{
+  allContentstackBlog {
+    edges {
+      node {
+        owner {
+          author_name
+          birth_place
+          online_references {
+            links {
+              title
+            }
+            pdf {
+              title
+            }
+          }
+        }
+        authors {
+          first_name
+          last_name
+        }
+      }
+    }
+  }
+}`;
